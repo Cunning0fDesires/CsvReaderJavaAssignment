eb058c22af53869d29c2cfdc1721dcec2f5ad7e8
Yulia <ysosnina1@gmail.com>
2023-12-02 22:00:42 +0000

    sql script

--- consoleapp/src/main/java/org/example/App.java
+++ consoleapp/src/main/java/org/example/App.java
@@ -9,7 +9,6 @@ import org.example.service.FileReaderService;
 import org.example.util.ConsoleInterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class App {
