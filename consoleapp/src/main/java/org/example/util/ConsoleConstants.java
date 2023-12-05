29fb5c4af2e7c5d7b80fe40f333a39170f3fffbe
Yulia <ysosnina1@gmail.com>
2023-12-05 00:57:54 +0000

    extracted constants

---
+++ consoleapp/src/main/java/org/example/util/ConsoleConstants.java
@@ -0,0 +1,11 @@
+package org.example.util;
+
+public class ConsoleConstants {
+    public static final String INITIAL_WELCOMING = "Welcome to the console program to parse and save files!";
+    public static final String ENTER_FILE_LOCATION_PROMPT = "Please enter the location of the file you want to process";
+    public static final String GOODBYE = "Thank you for using the program, see you next time!";
+    public static final String JSON_SAVING_SUCCESS = "Done! The file has been saved as JSON. If you want to leave, please enter 'exit'";
+    public static final String NO_DATA_ERROR = "Oops! It seems your file has no data. Please check your file and try again.";
+    public static final String UNKNOWN_ERROR = "Something went wrong, please try again or change the input file.";
+    public static final String EXIT_WORD = "exit";
+}
--- consoleapp/src/main/java/org/example/util/ConsoleInterface.java
+++ consoleapp/src/main/java/org/example/util/ConsoleInterface.java
@@ -6,37 +6,36 @@ import org.example.exceptioins.MissingInputDataException;
 import org.example.service.CustomerClientService;
 import org.example.service.FileReaderService;
 
-
 import java.util.List;
 import java.util.Scanner;
 
 @AllArgsConstructor
 public class ConsoleInterface {
+
     private final FileReaderService<CustomerDto> fileReader;
     private final CustomerClientService customerClientService;
-    public static final String EXIT_WORD = "exit";
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Welcome to the console program to parse and save files!"
+        System.out.print(ConsoleConstants.INITIAL_WELCOMING
                 + System.lineSeparator()
-                + "Please enter the location of the file you want to process" + System.lineSeparator());
+                + ConsoleConstants.ENTER_FILE_LOCATION_PROMPT + System.lineSeparator());
         while (true) {
             String userInput = scanner.nextLine();
 
-            if (userInput.equalsIgnoreCase(EXIT_WORD)) {
-                System.out.println("Thank you for using the program, see you next time!");
+            if (userInput.equalsIgnoreCase(ConsoleConstants.EXIT_WORD)) {
+                System.out.println(ConsoleConstants.GOODBYE);
                 break;
             } else {
                 try {
                     List<CustomerDto> customerDtos = fileReader.readFile(userInput);
+                    System.out.println(customerDtos);
                     customerClientService.uploadCustomer(customerDtos);
-                    System.out.println("Done! The file has been saved as JSON. If you want to leave, please enter 'exit'" + System.lineSeparator());
+                    System.out.println(ConsoleConstants.JSON_SAVING_SUCCESS + System.lineSeparator());
                 } catch (MissingInputDataException e) {
-                    System.out.println("Oops! It seems your file has no data. Please check your file and try again." + System.lineSeparator());
-                }
-                catch (Exception e) {
-                    System.out.println("Something went wrong, please try again or change the input file.");
+                    System.out.println(ConsoleConstants.NO_DATA_ERROR + System.lineSeparator());
+                } catch (Exception e) {
+                    System.out.println(ConsoleConstants.UNKNOWN_ERROR);
                 }
             }
         }
