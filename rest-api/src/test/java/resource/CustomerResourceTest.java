4905d99828d12a9864fdbc7a09e815520828673f
Yulia <ysosnina1@gmail.com>
2023-12-05 00:59:32 +0000

    refactored a customer service

--- rest-api/src/test/java/resource/CustomerResourceTest.java
+++ rest-api/src/test/java/resource/CustomerResourceTest.java
@@ -1,9 +1,9 @@
 package resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.repository.CustomerRepository;
-import org.example.repository.entities.*;
+import org.example.dto.CustomerDto;
 import org.example.resource.CustomerResource;
+import org.example.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,18 +17,16 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.*;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerResourceTest {
 
     @Mock
-    private CustomerRepository customerRepository;
+    private CustomerService customerService;
     @InjectMocks
     private CustomerResource customerResource;
 
@@ -43,50 +41,50 @@ public class CustomerResourceTest {
 
     @Test
     public void uploadCsvFile() throws Exception {
-        CustomerEntity customer = buildMockCustomer();
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/customers/upload")
+        CustomerDto customerDto = buildMockCustomerDto();
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/customers")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(Collections.singletonList(customer))))
+                        .content(objectMapper.writeValueAsString(Collections.singletonList(customerDto))))
                 .andExpect(status().isOk());
-        
-        verify(customerRepository).saveAll(Collections.singletonList(customer));
     }
 
     @Test
     public void getCustomerByReference() throws Exception {
-            CustomerEntity mockCustomer = buildMockCustomer();
-            UUID customerRef = mockCustomer.getCustomerRef();
-        
-          when(customerRepository.findById(eq(customerRef))).thenReturn(Optional.of(mockCustomer));
-         
-                 mockMvc.perform(MockMvcRequestBuilders.get("/api/customers/{customerRef}", customerRef)
-                         .contentType(MediaType.APPLICATION_JSON))
-                         .andExpect(MockMvcResultMatchers.status().isOk())
-                         .andExpect(MockMvcResultMatchers.jsonPath("$.customerRef").value(customerRef.toString()))
-                         .andExpect(MockMvcResultMatchers.jsonPath("$.customerName").value("Mock Customer"));
+        CustomerDto mockCustomer = buildMockCustomerDto();
+        UUID customerRef = UUID.fromString(mockCustomer.getCustomerRef());
+
+        lenient().when(customerService.findByCustomerReference(eq(customerRef))).thenReturn(mockCustomer);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/customers/{customerRef}", customerRef)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("customerRef").value(customerRef.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("customerName").value("Mock Customer"));
     }
 
     @Test
     public void getNonExistingCustomer() throws Exception {
         UUID customerRef = UUID.randomUUID();
 
-        when(customerRepository.findById(eq(customerRef))).thenReturn(Optional.empty());
+        lenient().when(customerService.findByCustomerReference(eq(customerRef))).thenReturn(null);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/customers/{customerRef}", customerRef)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(""));
+
     }
 
-    private CustomerEntity buildMockCustomer() {
-        return CustomerEntity.builder()
-                .customerRef(UUID.randomUUID())
+    private CustomerDto buildMockCustomerDto() {
+        return CustomerDto.builder()
+                .customerRef(UUID.randomUUID().toString())
                 .customerName("Mock Customer")
-                .addressLine1(AddressLineEntity.builder().addressLine("Address 11").build())
-                .town(TownEntity.builder().townName("Putney").build())
-                .country(CountryEntity.builder().countryName("UK").build())
-                .county(CountyEntity.builder().countyName("London").build())
-                .postcode(PostcodeEntity.builder().postcode("183389389hfj").build())
+                .county("London")
+                .country("UK")
+                .addressLine1("Address 1")
+                .addressLine2("Address 2")
+                .postcode("147bvc")
+                .town("Putney")
                 .build();
     }
 }
