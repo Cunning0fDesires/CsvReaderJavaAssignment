a1664ca1ee8fd11adc7c2693f6a7bf96648ee421
Yulia <ysosnina1@gmail.com>
2023-12-05 00:59:27 +0000

    refactored a customer service

--- rest-api/src/main/java/org/example/service/CustomerService.java
+++ rest-api/src/main/java/org/example/service/CustomerService.java
@@ -3,17 +3,19 @@ package org.example.service;
 import org.example.dto.CustomerDto;
 import org.example.mappers.CustomerMapper;
 import org.example.repository.CustomerRepository;
-import org.example.repository.entities.CustomerEntity;
+import org.example.repository.entities.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -22,10 +24,12 @@ public class CustomerService {
     public void saveCustomers(List<CustomerDto> customers) {
         List<CustomerEntity> entities = CustomerMapper.INSTANCE.toEntityList(customers);
         customerRepository.saveAll(entities);
+        log.info("Customers were saved successfully!");
     }
 
-    public Optional<CustomerDto> findByCustomerReference(UUID customerRef) {
-        Optional<CustomerEntity> customer = customerRepository.findById(customerRef);
-        return customer.map(CustomerMapper.INSTANCE::toDTO);
+    public CustomerDto findByCustomerReference(UUID customerRef) {
+        log.info("Looking for the customer that have UUID: " + customerRef);
+        CustomerEntity customer = customerRepository.findByCustomerRef(customerRef);
+        return CustomerMapper.INSTANCE.toDTO(customer);
     }
 }
